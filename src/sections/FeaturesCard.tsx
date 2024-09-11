9d0e6d0bef2ade68dea6185eea8df0b6821ff255
Adonis <adonis.casado@outlook.com>
2024-09-11 17:32:38 -0400

    grid

--- src/sections/FeaturesCard.tsx
+++ src/sections/FeaturesCard.tsx
@@ -1,4 +1,5 @@
 import { twMerge } from "tailwind-merge";
+import { LearnMoreButton } from "../components/LearnMoreButton";
 
 const cardData = [
   {
@@ -62,10 +63,7 @@ export const FeaturesCardsSection = () => {
               <h3 className="font-heading font-black text-3xl mt-12">{title}</h3>
               <p className="text-lg text-zinc-400 mt-4">{description}</p>
               <div className="flex justify-between mt-12">
-                <button className={twMerge("text-sm font-heading uppercase font-extrabold tracking-wider text-fuchsia-500", 
-                  color === 'lime' && 'text-lime-500',
-                  color === 'cyan' && 'text-cyan-500',
-                  color === 'violet' && 'text-violet-500')}>Learn More</button>
+                <LearnMoreButton color={color}>Learn More</LearnMoreButton>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
               </svg>
--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -1,3 +1,87 @@
+import { CutCornerButton } from "../components/CutCornerButton";
+import { LearnMoreButton } from "../components/LearnMoreButton";
+
+const listItems = [
+  'Experience unparalleled security and scalability',
+  'Fully benefit from scalable network effects',
+  'Unlock the potential of decentralized networks',
+];
+
+
 export const FeaturesGrid = () => {
-  return <div>Features Grid</div>
+  return (
+    <section className="py-24 overflow-x-clip">
+      <div className="container">
+        <div className="flex flex-col gap-56 md:gap-48">
+          <div className="grid grid-cols-3 gap-8">
+            <div className="col-span-2">
+              <h2 className="font-heading font-black text-4xl md:text-5xl">Empowering the future of blockchain.</h2>
+              <p className="text-xl text-zinc-400 mt-8">
+                Blockforge provides robust and secure infrastructure to support the next generation of decentralized applications.
+              </p>
+              <ul className="flex flex-col gap-8 mt-12">
+                {listItems.map((item) => (
+                  <li key={item} className="flex items-center gap-3">
+                    <div className="inline-flex flex-shrink-0 justify-center items-center size-8 outline outline-4 -outline-offset-4 outline-fuchsia-500/10 rounded-full">
+                      <div className="size-1.5 bg-fuchsia-500 rounded-full"></div>
+                    </div>
+                    <span className="text-xl font-bold">{item}</span>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex gap-8 mt-12">
+                <CutCornerButton>Get Started</CutCornerButton>
+                <LearnMoreButton>Learn More</LearnMoreButton>
+              </div>
+            </div>
+            <div>
+              <div className="relative inline-flex z-0">
+                <img 
+                src="/assets/images/torus-knot.png" 
+                alt="torus-knot 3d image"
+                className="size-96 max-w-none"
+                />
+                <img 
+                src="/assets/images/hemisphere.png"
+                alt="hemisphere 3d shape"
+                className="absolute size-96 top-3/4 -z-10 scale-x-[-1]"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-8">
+            <div className="relative">
+              <div className="absolute right-0 z-0">
+                  <img 
+                  src="/assets/images/cone.png" 
+                  alt="cone 3d shape" 
+                  className="size-96 max-w-none rotate-12"
+                  />
+                  <img
+                  src="/assets/images/hemisphere.png"
+                  alt="hemisphere 3D shape"
+                  className="absolute top-3/4 -z-10 left-0"
+                  />
+              </div>
+            </div>
+            <div className="col-span-2">
+                <h2 className="font-heading font-black text-4xl md:text-5xl">Blockforge leads the way.</h2>
+                <div className="flex flex-col text-xl text-zinc-400 mt-6 gap-6">
+                  <p>
+                    Blockforge is dedicated to supporting the revolution of Web3 applications by delivering the necessary infrastructure and security for Web3
+                  </p>
+                  <p>
+                  Blockforge champions Web3 for everyone. As a decentralized blockchain scaling platform, Blockforge enables developers to create scalable, user-friendly dApps with low transaction costs while ensuring robust security.
+                  </p>
+                </div>
+                <div className="flex gap-8 mt-12">
+                  <CutCornerButton>Get Started</CutCornerButton>
+                  <LearnMoreButton>Learn More</LearnMoreButton>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
