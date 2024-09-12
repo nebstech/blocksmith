a2b82f42dca464615e14e44286b8988c318e20be
Adonis <adonis.casado@outlook.com>
2024-09-11 20:39:55 -0400

    mobile posts

--- src/sections/FeaturesCard.tsx
+++ src/sections/FeaturesCard.tsx
@@ -1,4 +1,4 @@
-import { twMerge } from "tailwind-merge";
+import { Card } from "../components/Card";
 import { LearnMoreButton } from "../components/LearnMoreButton";
 
 const cardData = [
@@ -22,7 +22,7 @@ const cardData = [
   },
   {
     image: '/assets/images/icosahedron.png',
-    title: 'Seamless Blockchain Intergration',
+    title: 'Seamless Blockchain Integration',
     description: 'Integrating blockchain technology seamlessly into your projects, with minimal effort and maximum efficiency',
     color: 'violet',
   }
@@ -32,43 +32,28 @@ export const FeaturesCardsSection = () => {
   return (
     <section className="py-24 overflow-x-clip md:-mt-28">
       <div className="container">
-        <h2 className="font-headind font-black text-4xl text-center md:text-5xl lg:text-6xl">
+        <h2 className="font-heading font-black text-4xl text-center md:text-5xl lg:text-6xl">
           Discover the future of blockchain with Blockforge.
         </h2>
         <div className="mt-36 lg:mt-48 flex">
           <div className="flex flex-none gap-8">
-            {cardData.map(({ image, title, description, color}) => (
-              <div className="relative z-0 p-8 max-w-xs md:p-10 md:max-w-md group" key={title}>
-              <div 
-              className={twMerge("absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300", 
-              color === 'lime' && 'bg-lime-500',
-              color === 'cyan' && 'bg-cyan-500',
-              color === 'violet' && 'bg-violet-500')}>
-              </div>
-              <div className={twMerge("absolute size-16 rounded-xl bg-fuchsia-500 group-hover:bg-fuchsa-400 transition duration-300 top-1.5 right-1.5 -z-10", 
-                color === 'lime' && 'bg-lime-500 group-hover:bg-lime-400',
-                color === 'cyan' && 'bg-cyan-500 group-hover:bg-cyan-400',
-                color === 'violet' && 'bg-violet-500 group-hover:bg-violet-400')}></div>
-              <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
-              <div className="flex justify-center -mt-28">
-                <div className="inline-flex relative">
-                  <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
-                  <img 
-                  src={image} 
-                  alt="Pill image" 
-                  className="size-40 group-hover:-translate-y-6 transition duration-300"
-                  />
-              </div>
-              </div>
-              <h3 className="font-heading font-black text-3xl mt-12">{title}</h3>
-              <p className="text-lg text-zinc-400 mt-4">{description}</p>
-              <div className="flex justify-between mt-12">
-                <LearnMoreButton color={color}>Learn More</LearnMoreButton>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-              </svg>
-              </div>
-            </div>
+            {cardData.map(({ image, title, description, color }) => (
+              <Card key={title} color={color} className="max-w-xs md:max-w-md">
+                <div className="flex justify-center -mt-28">
+                  <div className="inline-flex relative">
+                    <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
+                    <img 
+                      src={image} 
+                      alt={title} 
+                      className="size-40 group-hover:-translate-y-6 transition duration-300"
+                    />
+                  </div>
+                </div>
+                <h3 className="font-heading font-black text-3xl mt-12">
+                  {title}
+                </h3>
+                <p className="text-lg text-zinc-400 mt-4">{description}</p>
+              </Card>
             ))}
           </div>
         </div>
---
+++ src/sections/LatestPosts.tsx
@@ -0,0 +1,31 @@
+import type { CollectionEntry } from "astro:content";
+import { Card } from "../components/Card";
+import { getPostColorFromCategory } from "../utils/postUtils";
+import { Tag } from "../components/Tag";
+
+export const LatestPosts = (props: {
+  latestPosts: CollectionEntry<'blog'>[];
+}) => {
+  const { latestPosts } = props;
+  return (
+    <section className="py-60">
+      <div className="container">
+        <h2 className="font-heading font-black text-4xl text-center">Your portal to everything blockchain.</h2>
+        <p className="text-xl text-center text-zinc-400 mt-8">
+          Keep up with the newes trends, updates, and insights in the blockchain world, updated weekly.
+        </p>
+        <div className="mt-16 flex flex-col gap-8">
+          {latestPosts.map(({ data: { title, description, category } }, postIndex) => (
+            <Card key={postIndex} buttonText="Read More" color={getPostColorFromCategory(category)}>
+              <Tag color={getPostColorFromCategory(category)}>{category}</Tag>
+              <h3 className="font-heading font-black text-3xl mt-4">{title}</h3>
+              <p className="text-lg text-zinc-400 mt-6">
+                {description}
+              </p>
+          </Card>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
