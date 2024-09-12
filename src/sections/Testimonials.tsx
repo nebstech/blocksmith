e55cd76f06c9ffbdceac78b7240c6a479b7df99a
Adonis <adonis.casado@outlook.com>
2024-09-12 02:36:33 -0400

    mobile testi

---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,50 @@
+const testimonials = [
+  {
+    text: 'The user experience is phenomenal, and the support team is always there to help. Highly recommended!',
+    name: 'Erica Wyatt',
+    title: 'Product Manager - BlockLink',
+    avatarImage: '/assets/images/avatar-erica-wyatt.jpg'
+  },
+  {
+    text: 'Our productivity has skyrocketed since we started using Blackforge. It\'s a game-changer for our team',
+    name: 'Noel Baldwin',
+    title: 'Lead Developer = BitBridge',
+    avatarImage: '/assets/images/avatar-noel-baldwin.jpg'
+  },
+  {
+    text: 'The intergration process was seamless and the performance improvements were beyond our expectations',
+    name: 'Harry Bender',
+    title: 'CTO - CryptoSolutions',
+    avatarImage: '/assets/images/avatar-harry-bender.jpg'
+  },
+  
+];
+
+export const TestimonialsSection = () => {
+  return (
+    <section className="py-32 bg-zinc-800">
+      <div className="container">
+        <div className="grid grid-cols-1 gap-16">
+          {testimonials.map((testimonial, testimonialIndex) => (
+              <blockquote key={testimonialIndex}>
+                <p className="font-heading text-3xl font-black">
+                  &ldquo;{testimonial.text}&rdquo;
+                </p>
+                <cite className="block mt-8">
+                  <div className="flex gap-3 items-center">
+                    <div>
+                      <div className="size-16 bg-zinc-700 rounded-full bg-cover" style={{ backgroundImage: `url(${testimonial.avatarImage})`,}}></div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="text-lg not-italic font-black">{testimonial.name}</div>
+                    <div className="not-italic text-zinc-400">{testimonial.title}</div>
+                  </div>
+                </cite>
+              </blockquote>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
