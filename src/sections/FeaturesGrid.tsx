702acbd3957a1c2490cba0247d3adcfb13a1193f
Adonis <adonis.casado@outlook.com>
2024-09-16 21:43:41 -0400

    build

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { CutCornerButton } from "../components/CutCornerButton";
 import { LearnMoreButton } from "../components/LearnMoreButton";
 import { motion, useScroll, useTransform } from "framer-motion";
